5c5b970a3973643f1b3601985da6ef6d79872e02
Bhuvana-shri-19 <bhuvanashri.ashok@gmail.com>
2025-12-26 19:44:54 +0530

    backend setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/apipulse")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.use("/auth", require("./routes/auth-temp"));
+app.use("/test", require("./routes/apiTest"));
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
